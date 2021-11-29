cb51b9a292485afdf179c4b1896b69159fae9121
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 16:34:13 +0530

    Add project files.

---
+++ module-five/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
